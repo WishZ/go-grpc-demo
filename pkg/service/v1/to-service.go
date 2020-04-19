a2ccb7ff757f7d2438f08a6c86258886c8bf1315
sunshine <1130018964@qq.com>
2020-04-19 15:48:53 +0800

    完善服务端接口

--- pkg/service/v1/to-service.go
+++ pkg/service/v1/to-service.go
@@ -3,10 +3,12 @@ package v1
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	v1 "github.com/WishZ/go-grpc-demo/api/proto/v1"
 	"github.com/golang/protobuf/ptypes"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"time"
 )
 
 const (
@@ -53,19 +55,159 @@ func (s toDoServiceServer) Create(ctx context.Context, request *v1.CreateRequest
 }
 
 func (s toDoServiceServer) Read(ctx context.Context, request *v1.ReadRequest) (*v1.ReadResponse, error) {
-	panic("implement me")
+	//检查版本
+	if err := s.checkApi(request.Api); err != nil {
+		return nil, err
+	}
+
+	//获取db连接
+	c, err := s.connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer c.Close()
+
+	rows, err := c.QueryContext(ctx, "SELECT * FROM m_todo WHERE `id` = ?", request.Id)
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "failed to select from m_todo->"+err.Error())
+	}
+	defer rows.Close()
+
+	if !rows.Next() {
+		if err := rows.Err(); err != nil {
+			return nil, status.Error(codes.Unknown, "failed to retrieve data from ToDo-> "+err.Error())
+		}
+		return nil, status.Error(codes.NotFound, fmt.Sprintf("m_todo with id = '%d' is not found", request.Id))
+	}
+
+	var td v1.ToDo
+	var reminder time.Time
+
+	if err := rows.Scan(&td.Id, &td.Title, &td.Description, &reminder); err != nil {
+		return nil, status.Error(codes.Unknown, "failed tod retrieve data from ToDo row =>"+err.Error())
+	}
+	td.Reminder, err = ptypes.TimestampProto(reminder)
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "reminder field has invalid format-> "+err.Error())
+	}
+	if rows.Next() {
+		return nil, status.Error(codes.Unknown, fmt.Sprintf("found multiple ToDo rows with ID='%d'", request.Id))
+	}
+
+	return &v1.ReadResponse{
+		Api:  apiVersion,
+		Todo: &td,
+	}, nil
 }
 
 func (s toDoServiceServer) Update(ctx context.Context, request *v1.UpdateRequest) (*v1.UpdateResponse, error) {
-	panic("implement me")
+	// 检查服务器是否支持客户端请求的API版本
+	if err := s.checkApi(request.Api); err != nil {
+		return nil, err
+	}
+	// 从池中获取sql连接
+	c, err := s.connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer c.Close()
+
+	reminder, err := ptypes.Timestamp(request.ToDo.Reminder)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, "reminder field has invalid format-> "+err.Error())
+	}
+
+	res, err := c.ExecContext(ctx, "UPDATE m_todo SET `title`=?,`Description` = ?,`reminder` = ? WHERE `id` = ?",
+		request.ToDo.Title, request.ToDo.Description, reminder, request.ToDo.Id)
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "数据更新失败："+err.Error())
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "获取更新行数失败："+err.Error())
+	}
+
+	if rows == 0 {
+		return nil, status.Error(codes.NotFound, fmt.Sprintf("更新的ID:%d不存在", request.ToDo.Id))
+	}
+
+	return &v1.UpdateResponse{
+		Api:     apiVersion,
+		Updated: rows,
+	}, nil
 }
 
 func (s toDoServiceServer) Delete(ctx context.Context, request *v1.DeleteRequest) (*v1.DeleteResponse, error) {
-	panic("implement me")
+	// 检查服务器是否支持客户端请求的API版本
+	if err := s.checkApi(request.Api); err != nil {
+		return nil, err
+	}
+	// 从池中获取sql连接
+	c, err := s.connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer c.Close()
+	// 删除ToDo
+	res, err := c.ExecContext(ctx, "DELETE FROM ToDo WHERE `ID`=?", request.Id)
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "failed to delete ToDo-> "+err.Error())
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "failed to retrieve rows affected value-> "+err.Error())
+	}
+	if rows == 0 {
+		return nil, status.Error(codes.NotFound, fmt.Sprintf("ToDo with ID='%d' is not found", request.Id))
+	}
+	return &v1.DeleteResponse{
+		Api:     apiVersion,
+		Deleted: rows,
+	}, nil
 }
 
 func (s toDoServiceServer) ReadAll(ctx context.Context, request *v1.ReadAllRequest) (*v1.ReadAllResponse, error) {
-	panic("implement me")
+	// 检查服务器是否支持客户端请求的API版本
+	if err := s.checkApi(request.Api); err != nil {
+		return nil, err
+	}
+	// 从池中获取sql连接
+	c, err := s.connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer c.Close()
+
+	// 获取ToDo列表
+	rows, err := c.QueryContext(ctx, "SELECT `ID`, `Title`, `Description`, `Reminder` FROM ToDo")
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "failed to select from ToDo-> "+err.Error())
+	}
+	defer rows.Close()
+
+	var reminder time.Time
+	var list []*v1.ToDo
+
+	for rows.Next() {
+		td := new(v1.ToDo)
+		if err := rows.Scan(&td.Id, &td.Title, &td.Description, reminder); err != nil {
+			return nil, status.Error(codes.Unknown, "failed to retrieve field values from ToDo row-> "+err.Error())
+		}
+
+		td.Reminder, err = ptypes.TimestampProto(reminder)
+
+		if err != nil {
+			return nil, status.Error(codes.Unknown, "reminder field has invalid format-> "+err.Error())
+		}
+		list = append(list, td)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, status.Error(codes.Unknown, "failed to retrieve data from ToDo-> "+err.Error())
+	}
+	return &v1.ReadAllResponse{
+		Api:   apiVersion,
+		ToDos: list,
+	}, nil
 }
 
 // NewToDoServiceServer创建ToDo服务
